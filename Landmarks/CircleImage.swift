4b20bee54dcba8690c98557bc9a2ddea4aa6b71e
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2024-03-12 09:59:24 +0500

    initializ image cirlce

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("eiffel-tower-sunset-summer-1024x676")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 
